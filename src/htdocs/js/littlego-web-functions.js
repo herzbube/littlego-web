25cd0a1c14b40afec50c46f053adaa977be1296a
Patrick Näf <herzbube@herzbube.ch>
2018-03-31 02:12:52 +0200

    add generalized function fractionalNumberToString(); komiToString() uses the new function

--- src/htdocs/js/littlego-web-functions.js
+++ src/htdocs/js/littlego-web-functions.js
@@ -52,22 +52,19 @@ function handicapToString(handicap)
 // for displaying in the UI. Throws an Error object for invalid values.
 function komiToString(komi)
 {
-    var entityFractionOneHalf = "&frac12;";
     switch (komi)
     {
         case 0:
             return "None";
         case 0.5:
-            return entityFractionOneHalf;
         case 5:
         case 6:
         case 7:
         case 8:
-            return "" + komi;
         case 5.5:
         case 6.5:
         case 7.5:
-            return "" + Math.floor(komi) + entityFractionOneHalf;
+            return fractionalNumberToString(komi);
         default:
             throw new Error("Unsupported komi value: " + komi);
     }
@@ -148,6 +145,49 @@ function startTimeToString(startTimeInMilliseconds)
             return "1 day ago"
         else
             return "" + elapsedTimeInDays + " days ago"
+    }
+}
+
+// Returns a nicely formatted HTML string for the specified fractional number.
+//
+// If the specified number is an integer, this function returns the number
+// converted to a string.
+//
+// If the specified number is a fractional type and the fraction is .0,
+// this function returns a string that omits the fraction.
+//
+// If the specified number is a fractional type and the fraction is not .0,
+// this function assumes that the fraction is .5. It returns a string that
+// represents the fraction with the HTML entity for "½".
+//
+/// A special case is 0.5: This number is represented with the integral part
+// omitted, i.e. 0.5 becomes "½", not "0½".
+function fractionalNumberToString(fractionalNumber)
+{
+    var flooredNumber = Math.floor(fractionalNumber);
+    if (flooredNumber === fractionalNumber)
+    {
+        // Math.floor() didn't change the type, so the specified
+        // number already was an integer. We can return it without
+        // change.
+        return "" + fractionalNumber;
+    }
+    else if (flooredNumber == fractionalNumber)
+    {
+        // Math.floor() did change the type, so the specified number
+        // was a fractional type, but the fraction apparently was .0.
+        // We return the floored value to make sure that string
+        // conversion doesn't include the fraction .0.
+        return "" + flooredNumber;
+    }
+    else
+    {
+        var entityFractionOneHalf = "&frac12;";
+
+        if (fractionalNumber === 0.5)
+            return entityFractionOneHalf;
+        else
+            return "" + flooredNumber + entityFractionOneHalf;
     }
 }
 
