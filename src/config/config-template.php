0e94bab11b78cf1443e7467db1821946274ec36b
Patrick Näf <herzbube@herzbube.ch>
2018-04-28 15:59:16 +0200

    add config-template.php (replaces config.php) and ignore local config.php

---
+++ src/config/config-template.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+// ----------------------------------------------------------------------
+// Notes:
+// - When this script is run it can expect that a configuration object
+//   is present in the $config variable.
+// - You don't need to explicitly configure everything. Only configure
+//   those options where you are not happy with the default values.
+// - To override a default value, simply uncomment the relevant line
+//   and set your own value.
+// ----------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------
+// WebSocket server configuration
+// ----------------------------------------------------------------------
+// $config->webSocketHost = "localhost";
+// $config->webSocketPort = "8001";
+
+?>
