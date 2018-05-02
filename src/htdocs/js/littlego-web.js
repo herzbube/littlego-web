e87fcac4c94cfb1fccd7987ef629959f637fe5e5
Patrick Näf <herzbube@herzbube.ch>
2018-05-02 03:35:23 +0200

    bugfix client: add missing "="

--- src/htdocs/js/littlego-web.js
+++ src/htdocs/js/littlego-web.js
@@ -341,7 +341,7 @@
         var numberOfColumns = NUMBER_OF_COLUMNS_GAME_REQUEST_TABLE;
         clearDataTableAndAddDataRetrievalPlaceholderMessage(appContainerID, numberOfColumns);
 
-        var messageData { };
+        var messageData = { };
         // Triggers onGetGameRequestsComplete
         sendWebSocketMessage(theWebSocket, WEBSOCKET_REQUEST_TYPE_GETGAMEREQUESTS, messageData);
     }
